79021901f17f5b0b28a4bc8ddc12f34659acb887
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-02-27 16:45:45 -0300

    estruturando o projeto

---
+++ src/components/Header/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './Layout.styles';
+
+const Layout = (props) => (
+  <div className="LayoutWrapper">
+    Test content
+  </div>
+);
+
+Layout.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Layout.defaultProps = {
+  // bla: 'test',
+};
+
+export default Layout;
