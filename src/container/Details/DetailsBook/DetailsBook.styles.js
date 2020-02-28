d43f90f2db0cadcaa4104959a52014fd0c1a850e
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-02-28 16:38:35 -0300

    Estruturando página de detalhes do livro

--- src/container/Details/DetailsBook/DetailsBook.styles.js
+++ src/container/Details/DetailsBook/DetailsBook.styles.js
@@ -1,6 +1,32 @@
-// import styled from 'styled-components';
+import styled from "styled-components";
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const Body = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Topo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ImageComponent = styled.div`
+  display: flex;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+`;
+
+
+export const InfoComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ButtonCarrinho = styled.div`
+  display: flex;
+`;
