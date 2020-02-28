1a1d84bf4a8bc2b1abfd6cd73b6362b4b918688f
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-02-28 16:44:48 -0300

    página de detalhes do livro atualizada

--- src/container/Details/DetailsBook/DetailsBook.jsx
+++ src/container/Details/DetailsBook/DetailsBook.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import { Test } from './DetailsBook.styles';
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Body,
+  Topo,
+  ImageComponent,
+  Image,
+  InfoComponent,
+  ButtonCarrinho
+} from "./DetailsBook.styles";
 
-const DetailsBook = (props) => (
-  <Layout>
-    sdfasdf
-  </Layout>
+const DetailsBook = props => (
+  <Body>
+    <Topo>
+      <ImageComponent>
+        <Image src={require("../../../assets/images/book1.png")} />
+      </ImageComponent>
+      <InfoComponent>
+        <span>Titulo</span>
+        <span>Autor</span>
+        <span>Ano</span>
+        <span>Situacao</span>
+        <span>Preco</span>
+      </InfoComponent>
+      <ButtonCarrinho>
+        <span>Adicionar ao Carrinho</span>
+      </ButtonCarrinho>
+    </Topo>
+    <div>
+      <div>SINOPSE</div>
+    </div>
+    <div>Avaliações</div>
+  </Body>
 );
 
 DetailsBook.propTypes = {
