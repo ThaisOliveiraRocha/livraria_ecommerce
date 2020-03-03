f7be3359972215324012d87d08385cdef9df8c92
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-03-03 10:59:10 -0300

    Bolinha quantidade itens exibe a soma da quantidade de itens no carrinho

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,35 +8,50 @@ import {
   QuantidadeItens
 } from "./Header.styles";
 
+import { connect } from "react-redux";
+
 import Button from "../../components/Button";
 import Titulo from "../../components/Title";
 import CartModal from "../CartModal";
 
-const Header = props => { 
+const totalCart = carrinho => {
+  const total = carrinho.reduce((aux, livro) => {
+    aux = aux + livro.qtd;
+    return aux;
+  }, 0);
+  return total;
+};
+
+const Header = ({ carrinho }) => {
   const [showModal, setModalStatus] = useState(false);
   return (
-  <Container>
-    <Image src={require("../../assets/images/book_novo.svg")} />
-    <Titulo font="28px" color="#ffffff" grow="1">
-      Livraria on-line
-    </Titulo>
-    <CartContainer onClick={() => setModalStatus(!showModal)}>
-      <Button>
-        <CartImage src={require("../../assets/images/carrinho_mercado.png")} />
-        Carrinho
-      </Button>
-      <QuantidadeItens>1</QuantidadeItens>
-    </CartContainer>
-    {showModal && <CartModal />}
-  </Container>
-)};
+    <Container>
+      <Image src={require("../../assets/images/book_novo.svg")} />
+      <Titulo font="28px" color="#ffffff" grow="1">
+        Livraria on-line
+      </Titulo>
+      <CartContainer onClick={() => setModalStatus(!showModal)}>
+        <Button>
+          <CartImage
+            src={require("../../assets/images/carrinho_mercado.png")}
+          />
+          Carrinho
+        </Button>
+        {carrinho.length > 0 && (
+          <QuantidadeItens>{totalCart(carrinho)}</QuantidadeItens>
+        )}
+      </CartContainer>
+      {showModal && <CartModal />}
+    </Container>
+  );
+};
 
 Header.propTypes = {
   // bla: PropTypes.string,
 };
 
-Header.defaultProps = {
-  // bla: 'test',
-};
+const mapStateToProps = state => ({
+  carrinho: state.livraria.carrinho
+});
 
-export default Header;
+export default connect(mapStateToProps)(Header);
