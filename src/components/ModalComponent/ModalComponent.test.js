8b49b9c83d5c8da25b15ebac663e03fd01498374
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-03-05 16:33:18 -0300

    Adicionando teste de verificação de clique no botão Cancel e Close

--- src/components/ModalComponent/ModalComponent.test.js
+++ src/components/ModalComponent/ModalComponent.test.js
@@ -13,13 +13,33 @@ describe("<ModalComponent />", () => {
     const wrapper = shallow(<ModalComponent tema={tema} mensagem={mensagem}/>);
     expect(wrapper).toMatchSnapshot();
   });
-  it("clicou no botão e chamou a função", () => {
+  it("clicou no botão >Confirma< e chamou a função", () => {
     const mock = jest.fn();
     const wrapper = shallow(
       <ModalComponent tema={tema} mensagem={mensagem} funcaoConfirma={mock} />
     );
 
     wrapper.find(".btnConfirmar").simulate("click");
+
+    expect(mock).toHaveBeenCalled();
+  });
+  it("clicou no botão >Cancel< e chamou a função", () => {
+    const mock = jest.fn();
+    const wrapper = shallow(
+      <ModalComponent tema={tema} mensagem={mensagem} cancel={mock} />
+    );
+
+    wrapper.find(".btnCancelar").simulate("click");
+
+    expect(mock).toHaveBeenCalled();
+  });
+  it("clicou no botão >Close< e chamou a função", () => {
+    const mock = jest.fn();
+    const wrapper = shallow(
+      <ModalComponent tema={tema} mensagem={mensagem} cancel={mock} />
+    );
+
+    wrapper.find(".close").simulate("click");
 
     expect(mock).toHaveBeenCalled();
   });
