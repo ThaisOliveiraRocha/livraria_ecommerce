dd7d034a3d1b7d1e436e4864d60da8d4946d2602
Thais <thais.rocha@dextra-sw.com>
2020-04-07 14:01:29 -0300

    Armazenando usuario logado no localStorage

--- cliente/src/api/index.js
+++ cliente/src/api/index.js
@@ -5,10 +5,10 @@ export const insertBook = (body) => axios.post("http://localhost:5001/books/inse
 export const updateBook = (body) => axios.put("http://localhost:5001/books/update-books", body);
 export const deleteBook = (body) => axios.post("http://localhost:5001/books/delete-books", body);
 
-
-
 export const authenticateUser = (body) => axios.post("http://localhost:5001/users/login", body);
-//export const updatePassword = (body) => axios.put(`http://localhost:5001/users/change-password`, body);
 export const updatePassword = (body) => axios.put(`http://localhost:5001/users/change-password/${body.email}`, body);
 export const insertNewUser = (body) => axios.post("http://localhost:5001/users/create", body);
 export const deleteUser = (body) => axios.delete(`http://localhost:5001/users/delete`, body);
+
+export const setUserSession = (info) => localStorage.setItem("userSession", info);
+export const getUserSession = () => localStorage.getItem("userSession");
